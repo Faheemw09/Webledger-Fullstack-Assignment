26587867773176df35323143a659c6bfed62424b
faheem hamid <faheemh712@gmail.com>
2023-10-19 22:43:01 +0530

    recipe page done now working on add to whishlist

--- Project_frontend/recipe_food_app/src/Pages/Recipe.jsx
+++ Project_frontend/recipe_food_app/src/Pages/Recipe.jsx
@@ -1,47 +1,74 @@
-//.jsx
-
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRecipe } from '../Redux/RecipeRedux/Action';
 import './Recipe.css';
 import Navbar from '../Components/Navbar';
-
+import { Button, Input, InputGroup, InputRightElement, Spinner } from '@chakra-ui/react';
 
 const Recipe = () => {
   const dispatch = useDispatch();
-  const recipes= useSelector((store) =>  store.Recipereducer.recipe)
- 
-  const {isLoading }= useSelector((state) => state.Authreducer.isLoading);
-
-console.log("recipes",recipes)
+  const recipes = useSelector((store) => store.Recipereducer.recipe);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const isLoading = useSelector((state) => state.Recipereducer.isLoading);
 
   useEffect(() => {
-    dispatch(fetchRecipe()); 
-  }, []);
+    dispatch(fetchRecipe(page, query))
+  }, [page, query]);
+
+  const debouncedQuery = useDebounce(query, 4000);
+
+  useEffect(() => {
+    if (debouncedQuery !== null) {
+      dispatch(fetchRecipe(page, debouncedQuery));
+    }
+  }, [page, debouncedQuery]);
 
   return (
     <>
-    <Navbar/>
-    <div className="recipe-list">
-    <div className='cards'>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        
-        recipes?.map((recipe, index) => (
-          
-            <div className="display_card">
-            <img src={recipe.image} alt={recipe.title} className="img2" />
-            <h2>{recipe.title}</h2>
-          </div>
-          
-        ))
-
-      )}
+      <Navbar />
+      <div className="recipe-list">
+        <div className='input'>
+          <input value={query} onChange={(e) => setQuery(e.target.value)} />
+          <div className='search-icon'>Search</div>
+        </div>
+        <div className='pagination'>
+          <button onClick={() => setPage(page - 1)} disabled={page === 1}>Prev</button>
+          <button>{page}</button>
+          <button onClick={() => setPage(page + 1)}>Next</button>
+        </div>
+        <div className='cardss'>
+          {isLoading ? (
+            <h1>Loading.............</h1>
+          ) : (
+            recipes?.map((recipe, index) => (
+              <div className="display_cards" key={recipe.id}>
+                <img src={recipe.image} alt={recipe.title} className="img5" />
+                <h2>{recipe.title}</h2>
+              </div>
+            ))
+          )}
+        </div>
       </div>
-    </div>
     </>
   );
 };
 
 export default Recipe;
+
+// Your useDebounce hook
+function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
--- Project_frontend/recipe_food_app/src/Redux/RecipeRedux/Action.js
+++ Project_frontend/recipe_food_app/src/Redux/RecipeRedux/Action.js
@@ -2,13 +2,23 @@ import axios from "axios"
 import { PENDING_REQUEST, SUCCESS_REQUEST ,FAILURE_REQUEST} from "./ActionTypes"
 
 
-const apiKey="32e5d9edaf8148d88f920bc5f55d420c"
+const apiKey="33650e18c02747399042f1786c38c03e"
 const url="https://api.spoonacular.com/recipes"
-export const fetchRecipe=()=>(dispatch)=>{
 
+
+export const fetchRecipe=(page,query)=>(dispatch)=>{
+    let limit=12
+    
     dispatch({type:PENDING_REQUEST})
    
-    axios.get(`${url}/complexSearch?apiKey=${apiKey}`)
+    axios.get(`${url}/complexSearch`,{
+        params: {
+            apiKey: apiKey,
+            number: limit,
+           offset: (page - 1) * limit,
+         query:query
+        }
+    })
     .then((res)=>{
         console.log("data",res.data.results)
         dispatch({type:SUCCESS_REQUEST,payload:res.data.results})
